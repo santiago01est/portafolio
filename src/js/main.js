eba94f7a934b25f47554ebb728cb4a2b74777e36
santiago01est <santiago01est@gmail.com>
2023-07-04 12:44:13 -0500

    add first version Contact section

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,9 @@
 //import * as bootstrap from 'bootstrap'
 
 /* Lottie*/
-
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Hola mundo");
+}
+const $form = document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
